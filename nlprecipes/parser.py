e3ef6c042a4e43dbeabf5b6ec580a57bc71a914c
LuoLei_Zhao <luoleizhao2018@u.northwestern.edu>
2017-02-22 17:38:43 -0600

    Parser now finds measurements in ingredients

--- nlprecipes/parser.py
+++ nlprecipes/parser.py
@@ -1,8 +1,10 @@
 import nltk
-#from ingredient import Ingredient
-#from direction import Direction
+from ingredient import Ingredient
+from direction import Direction
 from nltk.corpus import stopwords
+import re
 import string
+import recipe_lists
 
 stops = set(stopwords.words("english"))
 
@@ -12,9 +14,18 @@ def parse_ingredients(ingredientStrings):
 	for ingredientStr in ingredientStrings:
 		utf8 = ingredientStr.encode('utf8').lower().translate(None, string.punctuation)
 		tokenized = nltk.word_tokenize(utf8)
+		name, quantity, measurement, descriptor, preparation = "", "", "", "", ""
+		for token in tokenized:
+			for measure in recipe_lists.measurements:
+				measure_pattern = r'(?i)\b' + re.escape(measure)
+				if re.search(measure_pattern, token):
+					#print "measurement is: ", measure, " token is: " , token
+					measurement = token
+					break
+
 		tagged = nltk.pos_tag(tokenized)
-		#new_Ingredient = Ingredient(name, quantity, measurement, descriptor, preparation, unparsed)
-		#list_ingredients.append(new_Ingredient)
+		new_Ingredient = Ingredient(name, quantity, measurement, descriptor, preparation, ingredientStr)
+		list_ingredients.append(new_Ingredient)
 	return list_ingredients
 
 def parse_directions(directionStrings):
