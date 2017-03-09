1631062edbbe318ec5607c74e5cb9df6cc2bfddf
LuoLei_Zhao <luoleizhao2018@u.northwestern.edu>
2017-03-08 20:02:15 -0600

    Added instructions upon running the program

--- nlprecipes/main.py
+++ nlprecipes/main.py
@@ -3,7 +3,7 @@ import scraper
 import direction_parser
 import recipe
 import transformation
-def run(url):
+def run(url,transformation_type):
   print "Starting to get HTML"
   raw_html = scraper.get_html(url)
   print "parsing HTML"
@@ -11,9 +11,35 @@ def run(url):
   ingredients = parser.parse_ingredients(ingredients)
   directions = direction_parser.parse_directions(directions)
   recipes = recipe.Recipe(recipe_name, ingredients, directions)  
-  transformation.transform(recipes, 'vegetarian_to_meat')
+  transformation.transform(recipes, transformation_type)
   for d in ingredients:
   	print d.__dict__
   for d in directions:
   	print d.__dict__
+
+
+def parse(url, print_type):
+  print "Starting to get HTML"
+  raw_html = scraper.get_html(url)
+  print "parsing HTML"
+  ingredients, directions, recipe_name = scraper.parse_html(raw_html)
+  ingredients = parser.parse_ingredients(ingredients)
+  directions = direction_parser.parse_directions(directions)
+  recipes = recipe.Recipe(recipe_name, ingredients, directions)  
+  if print_type == "Object":
+    for d in ingredients:
+      print d.__dict__
+    for d in directions:
+      print d.__dict__
+  elif print_type == "Human":
+    print "Human readable string......"
+  return recipes
+
+def transform(recipes,transformation_type, print_type):
+  print "Performing Transformation"
+  transformation.transform(recipes, transformation_type)
+  if print_type == "Object":
+    print "object format....."
+  elif print_type == "Human":
+    print "Human readable string......"
  
